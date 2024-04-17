46e481f205b3825fff5de7df625a3bb42ab5b336
albert <angelalberto545@gmail.com>
2024-04-17 14:51:05 -0400

    Initial Commit

---
+++ GaminStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GaminStore
+//
+//  Created by Angel alberto Bernechea on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GaminStore/GaminStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaminStoreApp.swift
+//  GaminStore
+//
+//  Created by Angel alberto Bernechea on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GaminStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
